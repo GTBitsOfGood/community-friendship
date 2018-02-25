332fd8d12d80ef779bf5cfff5ea3828b025cdebc
Mya <myaetsang@gmail.com>
2018-02-25 12:59:10 -0500

    login page

--- backend/routes.js
+++ backend/routes.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 // YOUR API ROUTES HERE
+router.post('/login', (req,res) => {
+    res.send("the information you submitted is " + req.body.username + " " + req.body.password);
+});
+
+router.post('/register', (req,res) => {
+    res.send("the information you submitted is " + req.body.username + " " + req.body.password);
+});
 
 // SAMPLE ROUTE
-router.use('/users', (req, res) => {
-    res.json({ success: true });
-});
+// router.use('/users', (req, res) => {
+//     res.json({ success: true });
+// });
 
 module.exports = router;
