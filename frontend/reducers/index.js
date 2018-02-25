d81b11f4eb8b4f60486255be4683680ba4585772
EpicPi <piyushgk1@gmail.com>
2018-02-25 17:37:35 -0500

    start reeducer set up

--- frontend/reducers/index.js
+++ frontend/reducers/index.js
@@ -1,3 +1,10 @@
+import {combineReducers} from 'redux';
+import authreducer from './authreducer';
+
+export default combineReducers({
+    auth:authreducer,
+})
+
 function rootReducer(state = {name: 'Horizons'}, action) {
     switch (action.type) {
         default:
